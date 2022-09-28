fbd3b4f177582c7c6552415af68574fefe677d44
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2022-09-28 16:52:04 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import Mainlist from "./Mainlist";
+//import Doughnut from "./Doughnut";
+import Salespersonsdashboard from "./Salespersonsdashboard";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Salespersonsdashboard/>
+  </React.StrictMode>
+);
